adac1bae40d57dd1c4bb08c33dbd48eb870cffa0
Egor Sorokin <egor.v.s@mail.ru>
2022-12-28 22:04:08 +0300

    task2 created

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int dayOfTheWeekNumber = Prompt("Введите номер дня недели: ");
+string [] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+if (dayOfTheWeekNumber < 1 || dayOfTheWeekNumber > 7) Console.WriteLine("Такого дня недели не существует");
+else Console.WriteLine($"{days[dayOfTheWeekNumber - 1]} - {dayOfTheWeekNumber} день недели");
