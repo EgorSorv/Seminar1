4fabbeb578f6b233d75074cff26810c1d98d9b90
Egor Sorokin <egor.v.s@mail.ru>
2022-12-28 22:31:26 +0300

    task3 created

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine() ?? "0");
+    return value;
+}
+
+int value = Prompt("Введите число: ");
+int counter = -value;
+
+while (counter <= value)
+{
+    Console.Write(counter + " ");
+    counter++;
+}
