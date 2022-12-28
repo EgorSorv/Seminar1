d8d8597e0cea1cd533959c2742fbfc2ca8c9056f
Egor Sorokin <egor.v.s@mail.ru>
2022-12-28 18:38:22 +0300

    task0 created

---
+++ task0/Program.cs
@@ -0,0 +1,11 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+
+int value = Prompt("Введите число: ");
+int square = value * value;
+Console.WriteLine($"Квадрат числа {value} равен {square}");
