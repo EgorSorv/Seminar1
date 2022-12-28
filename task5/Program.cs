c0ab0988e34af1cf8fa811053a70b14d3dd0be45
Egor Sorokin <egor.v.s@mail.ru>
2022-12-28 23:22:41 +0300

    task5 created

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿double Prompt(string message)
+{
+    Console.Write(message);
+    double value = double.Parse(Console.ReadLine() ?? "0");
+    return value;
+}
+
+double value = Prompt("Введите число: ");
+Console.WriteLine("Обратным значением числа " + value + " будет число " + (1 / value));
