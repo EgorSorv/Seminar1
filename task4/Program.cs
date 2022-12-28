ad6a5cd997a6f8f10efe800929a07ab6ec06efcf
Egor Sorokin <egor.v.s@mail.ru>
2022-12-28 23:11:51 +0300

    task4 created

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = Prompt("Введите число: ");
+
+if (number >= 0) Console.WriteLine($"Модуль числа {number}  равен {number}");
+else Console.WriteLine($"Модуль числа {number} равен {-number}");
